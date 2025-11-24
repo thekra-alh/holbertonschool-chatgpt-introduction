f1465649bf078fbb87d2f828558fe10090e405a8
thekra-alh <202222146@yu.edu.sa>
2025-11-24 15:23:44 +0000

    l

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
-for arg in sys.argv[1:]:
-    print(arg)
+def main():
+    # اطبع كل الوسائط بدون اسم الملف
+    for arg in sys.argv[1:]:
+        print(arg)
+
+if __name__ == "__main__":
+    main()
